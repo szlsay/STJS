205c947fe7a225ee9d3aecd711bbff409a428873
szlsay <409178030@qq.com>
2021-12-02 10:59:43 +0800

    Create 7.1.js

---
+++ Professional4/07/7.1.js
@@ -0,0 +1,11 @@
+// for (let i = 1; i <= 10; ++i) {
+//   console.log(i);
+// }
+
+// let collection = ["foo", "bar", "baz"];
+// for (let index = 0; index < collection.length; ++index) {
+//   console.log(collection[index]);
+// }
+
+let collection = ['foo', 'bar', 'baz'];
+collection.forEach((item) => console.log(item));
