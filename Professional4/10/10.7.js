8fe6b1d44a2191d83b0ad73ad4f3b86cbec179a8
szlsay <409178030@qq.com>
2021-12-04 15:37:06 +0800

    Create 10.7.js

---
+++ Professional4/10/10.7.js
@@ -0,0 +1,17 @@
+// // 没问题
+// console.log(sum(10, 10));
+// function sum(num1, num2) {
+//   return num1 + num2;
+// }
+
+// // 会出错
+// console.log(sum(10, 10));
+// let sum = function (num1, num2) {
+//   return num1 + num2;
+// };
+
+// // TypeError: sum is not a function
+// console.log(sum(10, 10));
+// var sum = function (num1, num2) {
+//   return num1 + num2;
+// };
