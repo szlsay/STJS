1b431cae8228b7ce57745ec262191c26d1150435
szlsay <409178030@qq.com>
2021-12-04 14:39:03 +0800

    Create 10.4.js

---
+++ Professional4/10/10.4.js
@@ -0,0 +1,16 @@
+// function addSomeNumber(num) {
+//   return num + 100;
+// }
+// function addSomeNumber(num) {
+//   return num + 200;
+// }
+// let result = addSomeNumber(100); // 300
+
+// let addSomeNumber = function (num) {
+//   return num + 100;
+// };
+// addSomeNumber = function (num) {
+//   return num + 200;
+// };
+// let result = addSomeNumber(100); // 300
+// console.log(result);
