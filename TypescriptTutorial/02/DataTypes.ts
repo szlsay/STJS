05677714d7c50bd85ddce4a7a8628ab53376a1c6
szlsay <409178030@qq.com>
2021-12-08 09:50:17 +0800

    Create DataTypes.ts

---
+++ TypescriptTutorial/02/DataTypes.ts
@@ -0,0 +1,44 @@
+// let isDone: boolean = false;
+
+// let createdByNewBoolean: boolean = new Boolean(1); //不能将类型“Boolean”分配给类型“boolean”。
+
+// let createdByNewBoolean: Boolean = new Boolean(1);
+
+// let decLiteral: number = 6;
+// let hexLiteral: number = 0xf00d;
+// // ES6 中的二进制表示法
+// let binaryLiteral: number = 0b1010;
+// // ES6 中的八进制表示法
+// let octalLiteral: number = 0o744;
+
+// let notANumber: number = NaN;
+// let infinityNumber: number = Infinity;
+
+// let myName: string = "Tom";
+// let myAge: number = 25;
+
+// // 模板字符串
+// let sentence: string = `Hello, my name is ${myName}.
+// I'll be ${myAge + 1} years old next month.`;
+// console.log(sentence);
+
+// function alertName(): void {
+//   alert("My name is Tom");
+// }
+
+// let unusable: void = undefined;
+
+// let u: undefined = undefined;
+// let n: null = null;
+
+// // 这样不会报错
+// let num: number = undefined;
+
+// // 这样也不会报错
+// let u: undefined;
+// let num: number = u;
+
+// let u: void;
+// let num: number = u;
+
+// Type 'void' is not assignable to type 'number'.
